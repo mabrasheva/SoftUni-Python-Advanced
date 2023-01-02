6fbf21a9760b06d234b8c3bd8e0a0f7571bd2b0e
Maria Mihaleva <mabrasheva1@gmail.com>
2023-01-02 17:12:10 +0200

    Added easter_bunny.py.

---
+++ Multidimensional_Lists/easter_bunny.py
@@ -0,0 +1,110 @@
+"""
+Your task is to collect as many eggs as possible.
+On the first line, you will be given a number representing the size of the field.
+On the following few lines, you will be given a field with:
+•	One bunny - randomly placed in it and marked with the symbol "B"
+•	Number of eggs placed at different positions of the field and traps marked with "X"
+Your job is to determine the direction in which the bunny should go to collect the maximum number of eggs.
+The directions that should be considered as possible are up, down, left, and right.
+If you reach a trap while checking some of the directions, you should not consider the fields after the trap in this
+direction. For more clarifications, see the examples below.
+Note: Consider ONLY the paths from which the bunny has collected 1 or more eggs.
+Input
+•	A number representing the size of the field
+•	The matrix representing the field (each position separated by a single space)
+Output
+•	The direction which should be considered as best (lowercase)
+•	The field positions from which we are collecting eggs as lists
+•	The total number of eggs collected
+Constraints
+•	There will NOT be two or more paths consisting of the same total amount of eggs.
+"""
+
+
+# import sys
+# from io import StringIO
+#
+# test_input1 = """5
+# 1 3 7 9 11
+# X 5 4 X 63
+# 7 3 21 95 1
+# B 1 73 4 9
+# 9 2 33 2 0
+# """
+# test_input2 = """8
+# 4 18 9 7 24 41 52 11
+# 54 21 19 X 6 34 75 57
+# 76 67 7 44 76 27 56 37
+# 92 35 25 37 52 34 56 72
+# 35 X 1 45 4 X 37 63
+# 105 X B 2 12 43 5 19
+# 48 19 35 20 32 27 42 4
+# 73 88 78 32 37 52 X 22
+# """
+# test_input3 = """5
+# B -3 X 9 11
+# 0 5 4 X 63
+# X 3 21 95 1
+# 0 1 73 4 9
+# 9 2 33 2 0
+# """
+# sys.stdin = StringIO(test_input1)
+# sys.stdin = StringIO(test_input2)
+# sys.stdin = StringIO(test_input3)
+
+
+def bunnie_position(size_matrix):
+    for row in range(size_matrix):
+        for column in range(size_matrix):
+            if matrix[row][column] == "B":
+                return row, column
+
+
+def get_children(direction, bunnie_row, bunnie_column, size):
+    children = []
+    current_sum = 0
+    row, column = 0, 0
+
+    if direction == "right":
+        row, column = 0, 1
+    elif direction == "left":
+        row, column = 0, -1
+    elif direction == "up":
+        row, column = -1, 0
+    elif direction == "down":
+        row, column = 1, 0
+
+    for _ in range(size):
+        bunnie_row += row
+        bunnie_column += column
+        if 0 <= bunnie_row < size and 0 <= bunnie_column < size:
+            if matrix[bunnie_row][bunnie_column] == "X":
+                break
+            if int(matrix[bunnie_row][bunnie_column]) >= 0:
+                children.append([bunnie_row, bunnie_column])
+                current_sum += int(matrix[bunnie_row][bunnie_column])
+
+    return children, current_sum
+
+
+size_matrix = int(input())
+matrix = []
+for row in range(size_matrix):
+    matrix.append([i for i in input().split()])
+
+bunnie_row, bunnie_column = bunnie_position(size_matrix)
+directions = ["right", "left", "up", "down"]
+best_direction = ""
+max_sum = float("-inf")
+steps = []
+for direction in directions:
+    children, current_sum = get_children(direction, bunnie_row, bunnie_column, size_matrix)
+    if current_sum > max_sum and children:
+        max_sum = current_sum
+        best_direction = direction
+        steps = children
+
+print(best_direction)
+for element in steps:
+    print(element)
+print(max_sum)
