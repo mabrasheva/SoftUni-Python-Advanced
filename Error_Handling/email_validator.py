533466dacc49ee2fe6aa339facca02a75fae97ed
Maria Mihaleva <mabrasheva1@gmail.com>
2023-01-21 12:32:21 +0200

    Update email_validator.py.

--- Error_Handling/email_validator.py
+++ Error_Handling/email_validator.py
@@ -1,3 +1,16 @@
+"""You will be given some emails until you receive the command "End".
+Create the following custom exceptions to validate the emails:
+· NameTooShortError - raise it when the name in the email is less than or equal to 4 ("peter" will be the name in the
+email "peter@gmail.com")
+· MustContainAtSymbolError - raise it when there is no "@" in the email
+· InvalidDomainError - raise it when the domain of the email is invalid (valid domains are: .com, .bg, .net, .org)
+When an error is encountered, raise it with an appropriate message:
+· NameTooShortError - "Name must be more than 4 characters"
+· MustContainAtSymbolError - "Email must contain @"
+· InvalidDomainError - "Domain must be one of the following: .com, .bg, .org, .net"
+Hint: use the following syntax to add a message to the Exception: MyException("Exception Message")
+If the current email is valid, print "Email is valid" and read the next one"""
+
 import re
 
 
